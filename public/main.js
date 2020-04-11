819b3e7bf3a90da9af465efeda556cf8cca8a5a6
mrsovad <serov-pa@yandex.ru>
2020-04-12 01:25:38 +0300

    videochat

---
+++ public/main.js
@@ -0,0 +1,148 @@
+const server = 'https://127.0.0.1:3000';
+const video = document.querySelector('video');
+const filter = document.querySelector('#filter');
+const checkboxTheme = document.querySelector('#theme');
+
+let Peer = require('simple-peer');
+const socket = require('socket.io-client')(server);
+
+let client = {};
+let currentFilter;
+//get stream
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then(stream => {
+        socket.emit('newClient');
+        video.srcObject = stream;
+        video.play();
+
+        filter.addEventListener('change', (event) => {
+            currentFilter = event.target.value;
+            video.style.filter = currentFilter;
+            sendFilter(currentFilter);
+            event.preventDefault;
+        });
+
+        // used to initialize a peer
+        function initPeer(type) {
+            let peer = new Peer({
+                initiator: type === 'init',
+                stream:stream,
+                trickle:false
+            });
+            peer.on('stream', function (stream) {
+                createVideo(stream);
+            });
+            peer.on('data', function (data) {
+                let decodedData = new TextDecoder('utf-8').decode(data);
+                let peervideo = document.querySelector('#peerVideo');
+                peervideo.style.filter = decodedData;
+            });
+            return peer
+        }
+
+        // for peer of type init
+        function makePeer() {
+            client.gotAnswer = false;
+            let peer = initPeer('init');
+
+            peer.on('signal', function(data) {
+                if (!client.gotAnswer) {
+                    socket.emit('offer', data);
+                }
+            });
+            client.peer = peer;
+        }
+
+        // for peer of type not init
+        function frontAnswer(offer) {
+            let peer = initPeer('notInit');
+            peer.on('signal', (data) => {
+                socket.emit('answer', data);
+            });
+            peer.signal(offer);
+            client.peer = peer;
+        }
+
+        function signalAnswer(answer) {
+            client.gotAnswer = true;
+            let peer = client.peer;
+            peer.signal(answer);
+        }
+
+        function createVideo(stream) {
+            createDiv();
+            let video = document.createElement('video');
+            video.id = 'peerVideo';
+            video.srcObject = stream;
+            video.setAttribute('class', 'embed-responsive-item');
+            document.querySelector('#peerDiv').appendChild(video);
+            video.play();
+
+            //wait for 1 sec
+            setTimeout(() => sendFilter(currentFilter), 1000);
+
+            video.addEventListener('click', () => {
+                if (video.volume !== 0) {
+                    video.volume = 0;
+                } else {
+                    video.volume = 1;
+                }
+            });
+        }
+
+        function sessionActive() {
+            document.write('Session Active. Please come back later');
+        }
+
+
+        function removePeer() {
+            let videoClient = document.getElementById("peerVideo");
+            if (videoClient) {
+                document.getElementById("peerVideo").remove();
+            }
+            if (client.peer) {
+                client.peer.destroy()
+            }
+        }
+
+        function sendFilter(filter) {
+            if (client.peer) {
+                client.peer.send(filter)
+            }
+        }
+
+        socket.on('backOffer', frontAnswer);
+        socket.on('backAnswer', signalAnswer);
+        socket.on('sessionActive', sessionActive);
+        socket.on('createPeer', makePeer);
+        socket.on('disconnected', removePeer)
+    })
+    .catch(err => document.write(err));
+
+checkboxTheme.addEventListener('click', () => {
+        if (checkboxTheme.checked === true) {
+            document.body.style.backgroundColor = '#212529'
+            if (document.querySelector('#muteText')) {
+                document.querySelector('#muteText').style.color = "#fff"
+            }
+
+        }
+        else {
+            document.body.style.backgroundColor = '#fff'
+            if (document.querySelector('#muteText')) {
+                document.querySelector('#muteText').style.color = "#212529"
+            }
+        }
+    }
+);
+
+function createDiv() {
+    let div = document.createElement('div');
+    div.setAttribute('class', "centered");
+    div.id = "muteText";
+    div.innerHTML = "Click to Mute/Unmute";
+    document.querySelector('#peerDiv').appendChild(div);
+    if (checkboxTheme.checked === true) {
+        document.querySelector('#muteText').style.color = "#fff";
+    }
+}
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var http = require('http');
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+const app = express();
 
-var app = express();
-var server = http.createServer(app);
+const credentials = {
+    key: fs.readFileSync('docker/nginx/ssl/device.key', 'utf8'),
+    cert: fs.readFileSync('docker/nginx/ssl/chat.dv.crt', 'utf8')
+};
+let httpsServer = https.createServer(credentials, app);
+let io = require('socket.io')(httpsServer);
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,12 +16,40 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+let clients = [];
 
-app.get('/', function (req, res) {
-    res.send('Hello world');
+io.on('connection', function(socket){
+    socket.on('newClient', function() {
+        if (clients.length < 2) {
+            clients.push(socket.id);
+        } else {
+            this.emit('sessionActive');
+            return;
+        }
+        console.log(clients);
+        if (clients.length === 2) {
+            this.emit('createPeer');
+        }
+    });
+    socket.on('offer', sendOffer);
+    socket.on('answer', sendAnswer);
+    socket.on('disconnect', function() {
+        console.log('User left - '+ socket.id);
+        socket.broadcast.emit('disconnected');
+        let i = clients.indexOf(socket.id);
+        clients.splice(i, 1);
+    });
 });
 
-server.listen(3000);
-server.on('listening', function() {
-    console.log('Express server started on port %s', server.address().port);
+function sendOffer(offer) {
+    this.broadcast.emit("backOffer", offer);
+}
+
+function sendAnswer(data) {
+    this.broadcast.emit("backAnswer", data);
+}
+
+httpsServer.listen(3000);
+httpsServer.on('listening', function() {
+    console.log('Express server started on port %s', httpsServer.address().port);
 });
